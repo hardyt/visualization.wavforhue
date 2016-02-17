6b8a0dc997ff052ffb8e5cdf83c812d54c3d9aee
hardyt <odessa131@aol.com>
2016-02-17 02:31:58 -0700

    debugging multithreading

--- src/Main.cpp
+++ src/Main.cpp
@@ -18,230 +18,29 @@
  *
  */
 
-// Waveform.vis
-// A simple visualisation example by MrC
-
 
 //--------------------------------------------------------------------------------------
 #include <xbmc_vis_dll.h>
 #include <stdio.h>
-#ifdef HAS_OPENGL
-#ifdef __APPLE__
-#include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
-#else
-#include <GL/gl.h>
-#include <GL/glu.h>
-#endif
-#else
-#ifdef _WIN32
-#include <d3d11_1.h>
-#include <DirectXMath.h>
-#include <DirectXPackedVector.h>
-#include <stdio.h>
-#endif
-#endif
-
 //------------------------------------------------------------------
-#ifdef _WIN32
-#ifndef curl_socket_typedef
-/* socket typedef */
-#include <winsock2.h>
-typedef SOCKET curl_socket_t;
-#define CURL_SOCKET_BAD INVALID_SOCKET
-#endif
-#endif
 #include <atomic>
 #include <chrono>
-#include <iostream>
-#include <mutex>
-#include <queue>
 #include <thread>
-#include <curl/curl.h>
 //----------------------------------------------------------------------
 
-/*
-char g_visName[512];
-#ifndef HAS_OPENGL
-ID3D11Device*             g_device = NULL;
-ID3D11DeviceContext*      g_context = NULL;
-ID3D11VertexShader*       g_vShader = NULL;
-ID3D11PixelShader*        g_pShader = NULL;
-ID3D11InputLayout*        g_inputLayout = NULL;
-ID3D11Buffer*             g_vBuffer = NULL;
-ID3D11Buffer*             g_cViewPort = NULL;
 
-using namespace DirectX;
-using namespace DirectX::PackedVector;
-
-// Include the precompiled shader code.
-namespace
-{
-  #include "DefaultPixelShader.inc"
-  #include "DefaultVertexShader.inc"
-}
-
-struct cbViewPort
-{
-  float g_viewPortWidth;
-  float g_viewPortHeigh;
-  float align1, align2;
-};
-
-#else
-void* g_device;
-#endif
-float g_fWaveform[2][512];
-
-#ifdef HAS_OPENGL
-typedef struct {
-  int TopLeftX;
-  int TopLeftY;
-  int Width;
-  int Height;
-  int MinDepth;
-  int MaxDepth;
-} D3D11_VIEWPORT;
-typedef unsigned long D3DCOLOR;
-#endif
-
-D3D11_VIEWPORT g_viewport;
-
-struct Vertex_t
-{
-  float x, y, z;
-#ifdef HAS_OPENGL
-  D3DCOLOR  col;
-#else
-  XMFLOAT4 col;
-#endif
-};
-
-#ifndef HAS_OPENGL
-bool init_renderer_objs()
-{
-  // Create vertex shader
-  if (S_OK != g_device->CreateVertexShader(DefaultVertexShaderCode, sizeof(DefaultVertexShaderCode), nullptr, &g_vShader))
-    return false;
-
-  // Create input layout
-  D3D11_INPUT_ELEMENT_DESC layout[] =
-  {
-    { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT,    0,  0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-    { "COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-  };
-  if (S_OK != g_device->CreateInputLayout(layout, ARRAYSIZE(layout), DefaultVertexShaderCode, sizeof(DefaultVertexShaderCode), &g_inputLayout))
-    return false;
-
-  // Create pixel shader
-  if (S_OK != g_device->CreatePixelShader(DefaultPixelShaderCode, sizeof(DefaultPixelShaderCode), nullptr, &g_pShader))
-    return false;
-
-  // create buffers
-  CD3D11_BUFFER_DESC desc(sizeof(Vertex_t) * 512, D3D11_BIND_VERTEX_BUFFER, D3D11_USAGE_DYNAMIC, D3D11_CPU_ACCESS_WRITE);
-  if (S_OK != g_device->CreateBuffer(&desc, NULL, &g_vBuffer))
-    return false;
-
-  desc.ByteWidth = sizeof(cbViewPort);
-  desc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
-  desc.Usage = D3D11_USAGE_DEFAULT;
-  desc.CPUAccessFlags = 0;
-
-  cbViewPort viewPort = { (float)g_viewport.Width, (float)g_viewport.Height, 0.0f, 0.0f };
-  D3D11_SUBRESOURCE_DATA initData;
-  initData.pSysMem = &viewPort;
-
-  if (S_OK != g_device->CreateBuffer(&desc, &initData, &g_cViewPort))
-    return false;
-
-  // we are ready
-  return true;
-}
-#endif // !HAS_OPENGL
-//--------------------------------------------------------------------------------------
-*/
-
-
-//curl thread initialization -------------------------------------------------
-// this struct will go in the stack for the curl thread to read
-/*
-struct PutData
-{
-  std::string url;
-  std::string json;
-};
-*/
-//stack for putData - pushed by the main thread, top'ed and pop'ed by the curl thread
-//std::queue<PutData> gQueue;
-//std::queue<int> gQueue;
-//std::mutex gMutex;
+// thread initialization -------------------------------------------------
 std::atomic<bool> gRunThread(true);
 std::thread gWorkerThread;
-// end curl thread initialization ---------------------------------------------
-
-
-size_t noop_cb(void *ptr, size_t size, size_t nmemb, void *data) {
-  return size * nmemb;
-}
+// end  thread initialization ---------------------------------------------
 
 
 void workerThread()
 {
-  //PutData workerPutData;
-  //CURL *curl = curl_easy_init();
-  //CURLcode res;
-
-  //workerPutData.json = "{\"on\":true}";
-  //workerPutData.url = "http://192.168.10.6/api/KodiVisWave/lights/3/state";
-
   while (gRunThread)
   {
     std::this_thread::sleep_for(std::chrono::seconds(2));
-
-    //bool isEmpty = true;
-    /*
-    {
-      std::lock_guard<std::mutex> lock(gMutex);
-      bool isEmpty = gQueue.empty();
-    }
-    */
-    /*
-    if (isEmpty)
-    {
-      std::this_thread::sleep_for(std::chrono::seconds(2));
-    }
-    else
-    {
-      /*
-      std::lock_guard<std::mutex> lock(gMutex);
-      //PutData workerPutData = gQueue.front();
-      int value = gQueue.front();
-      gQueue.pop();
-
-      if (curl)
-      {
-        // Now specify we want to PUT data, but not using a file, so it has to be a CUSTOMREQUEST
-        curl_easy_setopt(curl, CURLOPT_TCP_NODELAY, 1);
-        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 3L);
-        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, noop_cb);
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, workerPutData.json.c_str());
-        // Set the URL that is about to receive our POST. 
-        curl_easy_setopt(curl, CURLOPT_URL, workerPutData.url.c_str());
-        // Perform the request, res will get the return code
-        res = curl_easy_perform(curl);
-      }
-      std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-    */
   }
-  /*
-  // always cleanup curl
-  if (curl)
-  {
-    curl_easy_cleanup(curl);
-  }
-  */
 }
 
 
@@ -254,25 +53,6 @@ ADDON_STATUS ADDON_Create(void* hdl, void* props)
 {
   if (!props)
     return ADDON_STATUS_UNKNOWN;
-  /*
-  VIS_PROPS* visProps = (VIS_PROPS*)props;
-
-#ifdef HAS_OPENGL
-  g_device = visProps->device;
-#endif
-  g_viewport.TopLeftX = visProps->x;
-  g_viewport.TopLeftY = visProps->y;
-  g_viewport.Width = visProps->width;
-  g_viewport.Height = visProps->height;
-  g_viewport.MinDepth = 0;
-  g_viewport.MaxDepth = 1;
-#ifndef HAS_OPENGL  
-  g_context = (ID3D11DeviceContext*)visProps->device;
-  g_context->GetDevice(&g_device);
-  if (!init_renderer_objs())
-    return ADDON_STATUS_PERMANENT_FAILURE;
-#endif
-    */
 
   return ADDON_STATUS_OK;
 }
@@ -291,28 +71,7 @@ extern "C" void Start(int iChannels, int iSamplesPerSec, int iBitsPerSample, con
 //-----------------------------------------------------------------------------
 extern "C" void AudioData(const float* pAudioData, int iAudioDataLength, float *pFreqData, int iFreqDataLength)
 {
-  /*
-  int ipos=0;
-  while (ipos < 512)
-  {
-    for (int i=0; i < iAudioDataLength; i+=2)
-    {
-      g_fWaveform[0][ipos] = pAudioData[i  ]; // left channel
-      g_fWaveform[1][ipos] = pAudioData[i+1]; // right channel
-      ipos++;
-      if (ipos >= 512) break;
-    }
-  }
-  */
-
-  /*
-  if (gQueue.size() < 10) //don't overload the queue
-  {
-    std::lock_guard<std::mutex> lock(gMutex);
-    //gQueue.push(mainPutData);
-    gQueue.push(1);
-  }
-  */
+ 
 }
 
 
@@ -321,94 +80,7 @@ extern "C" void AudioData(const float* pAudioData, int iAudioDataLength, float *
 //-----------------------------------------------------------------------------
 extern "C" void Render()
 {
-  /*
-  Vertex_t  verts[512];
 
-#ifndef HAS_OPENGL
-  unsigned stride = sizeof(Vertex_t), offset = 0;
-  g_context->IASetVertexBuffers(0, 1, &g_vBuffer, &stride, &offset);
-  g_context->IASetInputLayout(g_inputLayout);
-  g_context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_LINESTRIP);
-  g_context->VSSetShader(g_vShader, 0, 0);
-  g_context->VSSetConstantBuffers(0, 1, &g_cViewPort);
-  g_context->PSSetShader(g_pShader, 0, 0);
-  float xcolor[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
-#endif
-
-  // Left channel
-#ifdef HAS_OPENGL
-  GLenum errcode;
-  glColor3f(1.0, 1.0, 1.0);
-  glDisable(GL_BLEND);
-  glPushMatrix();
-  glTranslatef(0,0,-1.0);
-  glBegin(GL_LINE_STRIP);
-#endif
-  for (int i = 0; i < 256; i++)
-  {
-#ifdef HAS_OPENGL
-    verts[i].col = 0xffffffff;
-#else
-    verts[i].col = XMFLOAT4(xcolor);;
-#endif
-    verts[i].x = g_viewport.TopLeftX + ((i / 255.0f) * g_viewport.Width);
-    verts[i].y = g_viewport.TopLeftY + g_viewport.Height * 0.33f + (g_fWaveform[0][i] * g_viewport.Height * 0.15f);
-    verts[i].z = 1.0;
-#ifdef HAS_OPENGL
-    glVertex2f(verts[i].x, verts[i].y);
-#endif
-  }
-
-#ifdef HAS_OPENGL
-  glEnd();
-  if ((errcode=glGetError())!=GL_NO_ERROR) {
-    printf("Houston, we have a GL problem: %s\n", gluErrorString(errcode));
-  }
-#endif
-
-  // Right channel
-#ifdef HAS_OPENGL
-  glBegin(GL_LINE_STRIP);
-  for (int i = 0; i < 256; i++)
-#else
-  for (int i = 256; i < 512; i++)
-#endif
-  {
-#ifdef HAS_OPENGL
-    verts[i].col = 0xffffffff;
-    verts[i].x = g_viewport.TopLeftX + ((i / 255.0f) * g_viewport.Width);
-#else
-    verts[i].col = XMFLOAT4(xcolor);
-    verts[i].x = g_viewport.TopLeftX + (((i - 256) / 255.0f) * g_viewport.Width);
-#endif
-    verts[i].y = g_viewport.TopLeftY + g_viewport.Height * 0.66f + (g_fWaveform[1][i] * g_viewport.Height * 0.15f);
-    verts[i].z = 1.0;
-#ifdef HAS_OPENGL
-    glVertex2f(verts[i].x, verts[i].y);
-#endif
-  }
-
-#ifdef HAS_OPENGL
-  glEnd();
-  glEnable(GL_BLEND);
-  glPopMatrix();
-  if ((errcode=glGetError())!=GL_NO_ERROR) {
-    printf("Houston, we have a GL problem: %s\n", gluErrorString(errcode));
-  }
-#elif !defined(HAS_OPENGL)
-  // a little optimization: generate and send all vertecies for both channels
-  D3D11_MAPPED_SUBRESOURCE res;
-  if (S_OK == g_context->Map(g_vBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &res))
-  {
-    memcpy(res.pData, verts, sizeof(Vertex_t) * 512);
-    g_context->Unmap(g_vBuffer, 0);
-  }
-  // draw left channel
-  g_context->Draw(256, 0);
-  // draw right channel
-  g_context->Draw(256, 256);
-#endif
-  */
 }
 
 //-- GetInfo ------------------------------------------------------------------
@@ -475,23 +147,6 @@ extern "C" void ADDON_Stop()
 //-----------------------------------------------------------------------------
 extern "C" void ADDON_Destroy()
 {
-  /*
-#ifndef HAS_OPENGL
-  if (g_cViewPort)
-    g_cViewPort->Release();
-  if (g_vBuffer)
-    g_vBuffer->Release();
-  if (g_inputLayout)
-    g_inputLayout->Release();
-  if (g_vShader)
-    g_vShader->Release();
-  if (g_pShader)
-    g_pShader->Release();
-  if (g_device)
-    g_device->Release();
-#endif
-    */
-
   gRunThread = false;
   if (gWorkerThread.joinable())
   {
